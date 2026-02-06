367713072fddbd9f89a15a37af9a25616f5edf59
Bertrand THOMAS <bertrand@devpro.fr>
2026-02-06 22:19:01 +0100

    Update demo pipeline and samples

---
+++ samples/docker-nginx/versions.tf
@@ -0,0 +1,18 @@
+﻿terraform {
+  required_version = ">= 1.5"
+
+  # https://developer.hashicorp.com/terraform/language/settings/backends/http#configuration-variables
+  backend "http" {
+    lock_method            = "POST"
+    unlock_method          = "DELETE"
+    skip_cert_verification = "true"
+  }
+
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.6.2"
+    }
+  }
+}
