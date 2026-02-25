7c83d4b822948c376e8f7a4e253459ba0f2d0928
Bertrand THOMAS <bertrand@devpro.fr>
2026-02-25 18:45:05 +0100

    Verify openapi json file

--- test/WebApi.IntegrationTests/Resources/OpenApiResourceTest.cs
+++ test/WebApi.IntegrationTests/Resources/OpenApiResourceTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using VerifyXunit;
 using Xunit;
 
 namespace Devpro.TerraformBackend.WebApi.IntegrationTests.Resources;
@@ -20,7 +21,9 @@ public class OpenApiResourceTest(WebApplicationFactory<Program> factory)
         var response = await client.GetAsync("/openapi/v1.json", TestContext.Current.CancellationToken);
 
         // Assert
-        await CheckResponseAndGetContentAsync(response, HttpStatusCode.OK, "application/json; charset=utf-8",
+        var content = await CheckResponseAndGetContentAsync(response, HttpStatusCode.OK, "application/json; charset=utf-8",
             cancellationToken: TestContext.Current.CancellationToken);
+        await Verifier.VerifyJson(content)
+            .UseDirectory("../Snapshots");
     }
 }
