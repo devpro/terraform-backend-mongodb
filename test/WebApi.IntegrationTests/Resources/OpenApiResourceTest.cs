ace95c9f0075f742ceb7f05798de649b1914cbf7
Bertrand THOMAS <bertrand@devpro.fr>
2026-02-25 12:17:36 +0100

    Add OpenAPI resource test

---
+++ test/WebApi.IntegrationTests/Resources/OpenApiResourceTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Devpro.TerraformBackend.WebApi.IntegrationTests.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Devpro.TerraformBackend.WebApi.IntegrationTests.Resources;
+
+[Trait("Category", "IntegrationTests")]
+public class OpenApiResourceTest(WebApplicationFactory<Program> factory)
+    : IntegrationTestBase(factory)
+{
+    [Fact]
+    [Trait("Mode", "Readonly")]
+    public async Task OpenApiResource_Get_ReturnsOk()
+    {
+        // Arrange
+        var client = CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/openapi/v1.json");
+
+        // Assert
+        await response.CheckResponseAndGetContent(System.Net.HttpStatusCode.OK, "application/json; charset=utf-8");
+    }
+}
