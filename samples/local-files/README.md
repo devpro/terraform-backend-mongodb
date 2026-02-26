aabbae9e87da32e1dcf47a0d2cbf3e68a1ce6456
Bertrand THOMAS <bertrand@devpro.fr>
2026-02-26 15:04:05 +0100

    Update test

--- samples/local-files/README.md
+++ samples/local-files/README.md
@@ -24,9 +24,11 @@ cd samples/terraform-local-exec
 Initialize (feel free to use `tofu` instead of `terraform`):
 
 ```bash
-export TF_HTTP_ADDRESS="http://localhost:5293/dummy/state/local-files"
-export TF_HTTP_LOCK_ADDRESS="http://localhost:5293/dummy/state/local-files/lock"
-export TF_HTTP_UNLOCK_ADDRESS="http://localhost:5293/dummy/state/local-files/lock"
+# http://localhost:9001, http://localhost:5293
+export TFBACKEND_URL="http://localhost:9001"
+export TF_HTTP_ADDRESS="$TFBACKEND_URL/dummy/state/local-files"
+export TF_HTTP_LOCK_ADDRESS="$TFBACKEND_URL/dummy/state/local-files/lock"
+export TF_HTTP_UNLOCK_ADDRESS="$TFBACKEND_URL/dummy/state/local-files/lock"
 export TF_HTTP_USERNAME="admin"
 export TF_HTTP_PASSWORD="admin123"
 terraform init
--- test/WebApi.IntegrationTests/Resources/StateControllerResourceTest.cs
+++ test/WebApi.IntegrationTests/Resources/StateControllerResourceTest.cs
@@ -85,8 +85,8 @@ public class StateControllerResourceTest(WebApplicationFactory<Program> factory)
             "{\"message\":\"The state is locked.\"}", cancellationToken: TestContext.Current.CancellationToken);
 
         var wrongLockIdUpdateResponse = await client.PostAsync($"/{Tenant}/state/{name}?ID=1234", Serialize(state), TestContext.Current.CancellationToken);
-        await CheckResponseAndGetContentAsync(wrongLockIdUpdateResponse, HttpStatusCode.Conflict, "text/plain; charset=utf-8",
-            "LockId doesn't match with the existing lock", cancellationToken: TestContext.Current.CancellationToken);
+        await CheckResponseAndGetContentAsync(wrongLockIdUpdateResponse, HttpStatusCode.Locked, "application/json; charset=utf-8",
+            "{\"message\":\"The state is locked.\"}", cancellationToken: TestContext.Current.CancellationToken);
 
         var updateResponse = await client.PostAsync($"/{Tenant}/state/{name}?ID={stateLock.Id}", Serialize(state), TestContext.Current.CancellationToken);
         await CheckResponseAndGetContentAsync(updateResponse, HttpStatusCode.OK, null, string.Empty,
