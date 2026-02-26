c14fec9b00acb8304ed9519bc83c52e8778a7fa8
Bertrand THOMAS <bertrand@devpro.fr>
2026-02-26 21:04:48 +0100

    Add logs in UserRepository CheckAuthentication

--- src/Infrastructure.MongoDb/Repositories/UserRepository.cs
+++ src/Infrastructure.MongoDb/Repositories/UserRepository.cs
@@ -20,12 +20,21 @@ public class UserRepository : RepositoryBase, IUserRepository
 
     public async Task<UserModel?> CheckAuthentication(string username, string password)
     {
+        if (Logger.IsEnabled(LogLevel.Debug)) Logger.LogDebug("Checking authentication");
+
         var user = await _modelCollection.Find(x => x.Username == username).FirstOrDefaultAsync();
         if (user == null)
         {
+            if (Logger.IsEnabled(LogLevel.Information)) Logger.LogInformation("Authentication failed. Username {username} doesn't exist", username);
             return null;
         }
 
-        return BCrypt.Net.BCrypt.Verify(password, user.PasswordHash) ? user : null;
+        if (BCrypt.Net.BCrypt.Verify(password, user.PasswordHash))
+        {
+            return user;
+        }
+
+        if (Logger.IsEnabled(LogLevel.Information)) Logger.LogInformation("Authentication failed. Password for username {username} is incorrect", username);
+        return null;
     }
 }
